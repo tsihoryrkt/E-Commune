18d2ee31d999eefd0a4a0ccdd456531ac2fba24e
RAKOTOSON Tsihory Niaina <tsihoryrkt@gmail.com>
2024-06-27 21:44:20 +0300

    adding endpoint for connexion

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,16 +7,16 @@ const User = require('../models/User');
 // Configuration of the multer for images download
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'uploads/'); // Folder where images will be saved
+        cb(null, 'uploads/'); // Folder where images will be saved
     },
     filename: (req, file, cb) => {
-      cb(null, Date.now() + path.extname(file.originalname)); // unique name for the image
+        cb(null, Date.now() + path.extname(file.originalname)); // unique name for the image
     }
-  });
+});
 
 const upload = multer({ storage: storage });
 
-// Middleware to verifie the jeton
+// Middleware to verify the jeton
 function verifyToken(req, res, next) {
     const token = req.headers['authorization'];
     if (!token) return res.status(403).json({ message: 'No token provided' });
@@ -29,44 +29,74 @@ function verifyToken(req, res, next) {
     });
 }
 
-// Endpoint pour l'inscription
+// Endpoint for subscription
 const register = async (req, res) => {
     const { name, email, mobileNumber, password, isAdmin } = req.body;
     const image = req.file ? req.file.filename : null;
   
     try {
-      // Vérifier si l'email est déjà utilisé
+      // Vérify if the email is already used
       const existingEmail = await User.findOne({ email });
       if (existingEmail) {
         return res.status(400).json({ error: 'Email already in use' });
       }
   
-      // Vérifier si le numéro de téléphone est déjà utilisé
+      // Vérify if the phone number is already used
       const existingMobileNumber = await User.findOne({ mobileNumber });
       if (existingMobileNumber) {
         return res.status(400).json({ error: 'Mobile number already in use' });
       }
   
-      // Créer un nouvel utilisateur
+      // Create a new user
       const newUser = new User({
         name,
         email,
         mobileNumber,
         password,
-        isAdmin, // Ajouter l'attribut isAdmin
+        isAdmin, // Add attribut isAdmin
         image
       });
   
-      // Sauvegarder l'utilisateur dans la base de données
+      // Saving the user to the database
       await newUser.save();
       res.status(200).json({ message: 'Registration successful' });
-    } catch (error) {
+    } 
+    catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal server error' });
     }
   }
+
+//Endpoint for connexion
+const login = async (req, res) => {
+    const {email, password } = req.body;
+    
+    try {
+      // Find user via email
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+  
+      // Verify if password is valid
+      const isValid = await user.isValidPassword(password);
+      if (!isValid) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+  
+      // Generate a JWT token for the user
+      const token = jwt.sign({ userId: user._id, isAdmin: user.isAdmin }, 'your_jwt_secret', { expiresIn: '1h' });
+  
+      // send the response with the token and admin statut
+      res.json({ token, isAdmin: user.isAdmin });
+    }
+    catch (error) {
+      res.status(500).json({ message: 'Server error' });
+    }
+}
   
 module.exports = {
     register,
-    upload
+    upload,
+    login
 }
