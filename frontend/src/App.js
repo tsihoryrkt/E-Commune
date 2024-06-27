b7d15a4520467ff52a2bece065e30c6a44c58b77
RAKOTOSON Tsihory Niaina <tsihoryrkt@gmail.com>
2024-06-27 21:54:47 +0300

    adding react route for home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     
         <Route path="/register" Component={Register}/>
         <Route path="/login" Component={Login}/>
+        <Route path="/home" Component={Home}/>
       </Routes>
     </Router>
   );
