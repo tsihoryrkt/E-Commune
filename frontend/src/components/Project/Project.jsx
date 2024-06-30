09ba2e2be5e9a7fc8b021368b1057ce3be7e25bc
RAKOTOSON Tsihory Niaina <tsihoryrkt@gmail.com>
2024-06-30 23:38:38 +0300

    creating project frontend part done

--- frontend/src/components/Project/Project.jsx
+++ frontend/src/components/Project/Project.jsx
@@ -9,12 +9,27 @@ import '../../assets/css/Project.css';
 
 // import service
 import fetchUserData from "../../services/homeService";
+import { createProject } from "../../services/projectService";
 
 const Project = () => {
 
     const [ userData, setUserData ] = useState(null);
     const [ error, setError ] = useState('');
     const navigate = useNavigate();
+
+    const [ name,setName ] = useState('');
+    const [ description,setDescription ] = useState('');
+    const [ endDate,setEndDate ] = useState('');
+
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    // For searching project
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState('');
+    const [allProjects, setAllProjects] = useState([]);
+    const [selecteProject, setSelectedProject] = useState(null);
 
     
     useEffect(() => {
@@ -56,8 +71,45 @@ const Project = () => {
         }, 2000);
     };
 
+    const handleCreateUser = async (e) => {
+        setErrorMessage('');
+        setSuccessMessage('');
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('description', description);
+        formData.append('endDate', endDate);
+
+        const token = localStorage.getItem('token');
+
+        try {
+            await createProject(token, formData);
+                setErrorMessage('');
+                setSuccessMessage('Project created successfully');
+                setTimeout(() => {
+                    setName('');
+                    setDescription('');
+                    setEndDate('');
+                }, 2000);
+        }
+        catch (error) {
+            if (error.response && error.response.data && error.response.data.error) {
+                setErrorMessage(error.response.data.error);
+            } else {
+                setErrorMessage('An error occurred. Please try again.');
+            }
+            setSuccessMessage('');
+        };
+
+
+    };
+
     return (
-        <Navbar className="" expand="lg">
+        <div className="ProjectPage">
+            <div className="container">
+
+                <Navbar className="" expand="lg">
                     <Container>    
                         <Navbar.Brand href="/home">E-Commune</Navbar.Brand>
                         
@@ -72,15 +124,69 @@ const Project = () => {
                                 {userData.isAdmin && (
                                     <Nav.Link href="/task" className="text-dark">Tasks</Nav.Link>
                                     )}
+                                <Nav.Link href="/account" className="text-dark">Account</Nav.Link>
                             </Nav>
                             <Nav>
-                                <Nav.Link href="/account" className="text-dark">Account</Nav.Link>
                                 <Nav.Link onClick={handleLogout} className="text-dark">Logout</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
 
                     </Container>
                 </Navbar>
+                <div className="container-fluid d-flex justify-content-center align-items-center">
+                    <div className="container-fluid d-flex justify-content-center align-items-center NewProject">
+                        <div className="formDiv">
+                            <h1 className="display-6 fw-bold mb-4 mt-2 text-center">New Project</h1>
+                            {errorMessage && <p className="mt-3 text-danger errMess">{errorMessage}</p>}
+                            {successMessage && <p className="mt-3 text-success succMess">{successMessage}</p>}
+                            <form onSubmit={handleCreateUser} className="p-4 p-md-5 border rounded-3 bg-light">
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="form-floating mb-3">
+                                            <input type="text" className="form-control" value={name}  onChange={(e) => setName(e.target.value)} required/>
+                                            <label htmlFor="name">Name</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-floating mb-3">
+                                            <input type="text" className="form-control" value={description}  onChange={(e) => setDescription(e.target.value)}/>
+                                            <label htmlFor="name">Description</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-floating mb-3">
+                                            <input type="date" className="form-control" value={endDate}  onChange={(e) => setEndDate(e.target.value)}/>
+                                            <label htmlFor="endDate">End Date</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="submit" className="w-100 btn btn-lg btn-primary">Save</button>
+                           </form>
+                        </div>
+                    </div>
+
+                    <div className="AllProject mt-3">
+                        <div className="p-4 mt-3 border rounder-3 bg-light ProjectList">
+                            <div className="sticky-top d-flex align-items-center justify-content-between input-container">
+                                <input 
+                                    type="text" 
+                                    name="search_user" 
+                                    className="search-input"
+                                    placeholder="Search Project"
+                                    value={searchTerm}
+                                    onChange={(e) => setSearchTerm(e.target.value)}
+                                />
+                                <button className="search-button">
+                                    <FaSearch className="icon"/>
+                                </button>
+                            </div>
+                            <div className="mt-3 list-group">
+                            </div>
+                        </div>          
+                    </div>
+                </div>
+            </div>
+        </div>
 
     )
 }
---
+++ frontend/src/services/projectService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const createProject = async (token, formData) => {
+    try {
+        const response = await axios.post('http://localhost:5000/project/create', formData, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+        return response;
+    }
+    catch (error) {
+        throw error;
+    }  
+}
